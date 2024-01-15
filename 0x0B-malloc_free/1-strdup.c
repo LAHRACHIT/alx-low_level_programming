abb8308ad69fc1bae878c01e9abeb442410cd4e0
LAHRACHIT <azizlahrach.1992@gmail.com>
2024-01-15 00:38:58 -0800

    aziz

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _strdup - allocates space in memory
+*
+* @str: input string
+*
+* Return: returns a pointer to a newly
+* allocated space in memory
+*/
+
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int i, len;
+
+i = 0;
+len = 0;
+
+if (str == NULL)
+return (NULL);
+
+while (str[len])
+len++;
+
+dup = malloc(sizeof(char) * (len + 1));
+
+if (dup == NULL)
+return (NULL);
+
+while ((dup[i] = str[i]) != '\0')
+i++;
+
+return (dup);
+}
---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int _strlen(char *ch);
+
+/**
+* argstostr - concatenate arguments.
+* @ac: arg counter.
+* @av: arg vector.
+* Return: pointer p.
+*/
+char *argstostr(int ac, char **av)
+{
+int i, a = 0, k = 0;
+int j;
+char *p;
+if (ac == 0 || av == NULL)
+return (0);
+for (i = 0; i < ac; i++)
+{
+a += _strlen(av[i]);
+}
+p = malloc(a + 1 + ac);
+if (p == NULL)
+return (0);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; j < (_strlen(av[i])); j++)
+{
+p[k] = av[i][j];
+k++;
+}
+p[k] = '\n';
+k++;
+}
+p[k] = '\0';
+return (p);
+}
+
+/**
+* _strlen - counts number of chars.
+* @ch: string.
+* Return: pointer i.
+*/
+
+int _strlen(char *ch)
+{
+int i = 0;
+while (ch[i])
+i++;
+return (i);
+}
+
